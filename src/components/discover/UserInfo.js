160ea4623b76a12ac9f88b313647691103f524f7
AtnibamAitay <atnibamaitay@foxmail.com>
2024-03-05 22:27:33 +0800

    feat(UserInfo): 添加时间感知的问候语功能和UI样式调整
    - 引入`moment`库以获取当前时间，并根据时间动态显示用户问候语。
    - 在用户名前添加了基于当前时间计算得出的问候语内容。
    - 调整了`UserInfo`组件内`userInfoContainer`的样式，实现垂直居中对齐和间距优化。
    - 重新布局了用户头像容器，使其保持在右侧并水平居中对齐，同时调整了头像尺寸和边框宽度。
    - 更新了文本字体大小、颜色以及字体系列，改善了整体视觉效果。
    
    本次提交实现了根据当前时间向用户提供个性化问候语的功能，并对 UserInfo 组件进行了相应的样式改进与布局优化。

--- src/components/discover/UserInfo.js
+++ src/components/discover/UserInfo.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
+import moment from 'moment';
 
 const UserInfo = ({userInfo}) => {
   const {userAvatar, userName, userLocationRegion} = userInfo;
 
+  // 获取当前时间
+  const currentTime = moment().hours();
+
+  let greeting = '';
+
+  // 根据时间判断问候语
+  if (currentTime >= 6 && currentTime < 11) {
+    greeting = '早上好, ';
+  } else if (currentTime >= 11 && currentTime < 13) {
+    greeting = '中午好, ';
+  } else if (currentTime >= 13 && currentTime < 17) {
+    greeting = '下午好, ';
+  } else {
+    greeting = '晚上好, ';
+  }
+
   return (
     <View style={styles.userInfoContainer}>
-      <View style={styles.avatarAndTextContainer}>
-        <Text style={styles.userName}>{userName}</Text>
+      {/* 调整 userInfoContainer 的 justifyContent */}
+      <View style={[styles.textContainer, {marginVertical: 5}]}>
+        {/* 在用户名前添加问候语 */}
+        <Text style={styles.userName}>{`${greeting}${userName}`}</Text>
         <Text style={styles.location}>{userLocationRegion}</Text>
       </View>
 
-      <TouchableOpacity activeOpacity={0.8}>
-        <Image
-          source={{uri: userAvatar}}
-          style={[
-            styles.avatar,
-            {borderTopRightRadius: 0, borderBottomRightRadius: 0},
-          ]}
-        />
+      {/* 调整 avatarWrapper 并保持其在右边，但水平居中对齐 */}
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={[styles.avatarWrapper, {alignSelf: 'center'}]}>
+        <Image source={{uri: userAvatar}} style={styles.avatar} />
       </TouchableOpacity>
     </View>
   );
@@ -27,29 +43,32 @@ const UserInfo = ({userInfo}) => {
 const styles = {
   userInfoContainer: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
-  avatarAndTextContainer: {
+  textContainer: {
     flexDirection: 'column',
     justifyContent: 'center',
-    paddingRight: 10,
+    marginVertical: 2,
   },
   userName: {
     color: '#000000',
-    fontSize: 16,
-    fontWeight: 'bold',
+    fontSize: 26,
+    fontFamily: 'SmileySans-Oblique',
   },
   location: {
-    color: '#000000',
+    color: '#333333',
     fontSize: 14,
+    fontFamily: 'NotoSerifSC-Regular',
   },
   avatar: {
-    width: 50,
-    height: 50,
+    width: 40,
+    height: 40,
     borderRadius: 50,
-    borderWidth: 1,
+    borderWidth: 2,
     borderColor: 'white',
-    alignSelf: 'flex-end',
+  },
+  avatarWrapper: {
     marginRight: 10,
   },
 };
