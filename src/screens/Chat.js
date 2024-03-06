174280289755498a6cadeb82c1cd92a2627a9370
AtnibamAitay <atnibamaitay@foxmail.com>
2024-03-06 17:53:22 +0800

    feat(ChatScreen, API): 新增用户与AI聊天功能及消息发送逻辑
    - ChatScreen 组件中导入了 `useRef` 和多个 React Native UI 组件，并新增了 `ChatHeader`、`ChatBubble` 组件。
    - 添加了状态管理，包含输入文本(inputText)和聊天记录(chatMessages)的初始化及更新。
    - 定义了 `handleSend` 函数以处理用户消息的发送，包括将用户消息添加到聊天记录以及通过后端API与GPT模型交互获取回复。
    - 更新了 FlatList 用于显示聊天记录，包括自定义渲染器(renderItem)、keyExtractor 方法及滚动到底部逻辑。
    - 修改了样式表，增加了与聊天界面相关的样式规则。
    
    此次提交实现了用户与AI的实时聊天功能，包括消息的发送、接收和展示，同时优化了界面UI布局。

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,70 +1,160 @@
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, View, Text, Image, TextInput} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import BottomNavigation from '../components/common/BottomNavigation';
 import {useNavigation} from '@react-navigation/native';
+import {api} from '../../config';
 import LinearGradient from 'react-native-linear-gradient';
+
+const ChatHeader = () => (
+  <>
+    <Image
+      source={require('../../assets/icon/chat/polarbear.png')}
+      style={styles.circleImage}
+    />
+    <Text style={styles.title}>欢迎使用万象课堂</Text>
+    <Text style={styles.description}>
+      基于人工智能与苏格拉底哲学理论的强大教师，能够解答疑问、题目，引导学习。
+    </Text>
+    <View style={styles.separator} />
+  </>
+);
+
+const ChatBubble = ({item}) => (
+  <View
+    style={[
+      styles.chatBubble,
+      item.isUser ? styles.chatBubbleUser : styles.chatBubbleBot,
+      {alignSelf: item.isUser ? 'flex-end' : 'flex-start'},
+    ]}>
+    <Text
+      style={[
+        styles.chatBubbleText,
+        item.isUser ? styles.chatBubbleTextUser : styles.chatBubbleTextBot,
+      ]}>
+      {item.content}
+    </Text>
+  </View>
+);
 
 const Chat = () => {
   const navigation = useNavigation();
+  const [inputText, setInputText] = useState('');
+  const [chatMessages, setChatMessages] = useState([
+    {
+      type: 'header',
+    },
+    {
+      id: 0,
+      content: '有什么可以帮助你吗？',
+      isUser: false,
+    },
+  ]);
+  const flatListRef = useRef();
 
   useEffect(() => {}, []);
-  const gradientStyles = StyleSheet.create({
-    background: {
-      position: 'absolute',
-      zIndex: -1,
-      paddingHorizontal: 20,
-      alignItems: 'center',
-    },
-    gradient: {
-      start: {x: 0, y: 0},
-      end: {x: 1, y: 1},
-      colors: ['#FEF5FF', '#DFE4EE', '#D6E4FF'],
-    },
-  });
+
+  const handleSend = async () => {
+    if (inputText) {
+      const newUserMessage = {
+        id: chatMessages.length,
+        content: inputText,
+        isUser: true,
+      };
+      // 更新消息不直接改变状态，防止旧状态影响后续操作
+      setChatMessages(chatMessages => [...chatMessages, newUserMessage]);
+
+      try {
+        const response = await api.post('/ai/interact-with-gpt', [
+          {content: inputText, role: 'user'},
+        ]);
+
+        const newBotMessage = {
+          id: chatMessages.length + 1,
+          content:
+            response.status === 200 && response.data.code === 200
+              ? response.data.data
+              : '请重试',
+          isUser: false,
+        };
+
+        // 再次更新消息列表，加入从接口获取的回复或错误消息
+        setChatMessages(chatMessages => [...chatMessages, newBotMessage]);
+      } catch (error) {
+        console.error('Error during request:', error);
+        // 加入错误消息
+        setChatMessages(chatMessages => [
+          ...chatMessages,
+          {id: chatMessages.length + 1, content: '请重试', isUser: false},
+        ]);
+        Alert.alert('发送失败', '请检查网络连接或稍后再试');
+      }
+
+      setInputText('');
+    }
+  };
+
+  const renderItem = ({item}) => {
+    if (item.type === 'header') {
+      return <ChatHeader />;
+    }
+    return <ChatBubble item={item} />;
+  };
 
   return (
     <View style={styles.container}>
-      <View>
-        <View style={styles.imageContainer}>
-          <Image
-            source={require('../../assets/icon/chat/polarbear.png')}
-            style={[styles.circleImage, {resizeMode: 'contain'}]}
-          />
-        </View>
-
-        <Text style={styles.title}>欢迎使用万象课堂</Text>
-
-        <Text style={styles.description}>
-          基于人工智能与苏格拉底哲学理论的强大教
-        </Text>
-
-        <Text style={styles.description}>
-          师，能够解答疑问、题目，引导学习。
-        </Text>
-
-        {/* 水平分隔线 */}
-        <View style={styles.separator} />
-
-        {/* 默认消息 */}
-        <View style={styles.chatBubbleLeft}>
-          <Text style={styles.chatBubbleText}>有什么可以帮助你吗？</Text>
-        </View>
-      </View>
-
+      {/* 渐变背景 */}
       <LinearGradient
-        style={[styles.gradientBackground, gradientStyles.background]}
-        colors={gradientStyles.gradient.colors}
-        start={gradientStyles.gradient.start}
-        end={gradientStyles.gradient.end}>
-        {/* 文本输入框 */}
+        style={styles.gradientBackground}
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 1}}
+        colors={['#FEF5FF', '#DFE4EE', '#D6E4FF']}
+      />
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.keyboardAvoidingView}>
+        <FlatList
+          ref={flatListRef}
+          data={chatMessages}
+          renderItem={renderItem}
+          keyExtractor={item =>
+            item.type === 'header' ? 'header-key' : item.id?.toString()
+          } // 修改这里的keyExtractor函数
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.flatListContent}
+          onContentSizeChange={() =>
+            flatListRef.current.scrollToEnd({animated: true})
+          }
+          onLayout={() => flatListRef.current.scrollToEnd({animated: true})}
+        />
+      </KeyboardAvoidingView>
+
+      {/* 输入区域包括文本输入框和发送按钮 */}
+      <View style={styles.inputArea}>
         <TextInput
           style={styles.inputBox}
           placeholder="在此输入..."
           placeholderTextColor="#007AFF"
           underlineColorAndroid="transparent"
           selectionColor="#007AFF"
+          value={inputText}
+          onChangeText={setInputText}
         />
-      </LinearGradient>
+        <TouchableOpacity style={styles.sendButton} onPress={handleSend}>
+          <Text style={styles.sendButtonText}>发送</Text>
+        </TouchableOpacity>
+      </View>
 
       {/* 底部导航栏 */}
       <BottomNavigation currentRoute="Chat" navigation={navigation} />
@@ -77,12 +167,27 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 20,
   },
+  headerContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
   gradientBackground: {
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
+    backgroundColor: `linear-gradient(to right, ${'#FEF5FF'}, ${'#DFE4EE'}, ${'#D6E4FF'})`, // 使用提供的渐变颜色
+  },
+  keyboardAvoidingView: {
+    flex: 1,
+  },
+  inputArea: {
+    flexDirection: 'row',
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 40,
   },
   imageContainer: {
     alignItems: 'center',
@@ -95,8 +200,9 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
     borderColor: '#ffffff',
     borderWidth: 2,
-    marginTop: 70,
+    alignSelf: 'center',
   },
+
   title: {
     fontSize: 21,
     fontFamily: 'NotoSerifSC-Bold',
@@ -120,32 +226,86 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   inputBox: {
-    position: 'absolute',
-    bottom: 70,
+    flex: 1,
     height: 48,
-    width: '100%',
     paddingHorizontal: 16,
     borderWidth: 1,
     borderColor: '#007AFF',
-    backgroundColor: '#fff',
-    zIndex: 1,
+    backgroundColor: '#FFFFFF',
     borderRadius: 24,
+    marginRight: 12,
   },
   chatBubbleLeft: {
     backgroundColor: '#fff',
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-    borderRadius: 12,
     maxWidth: '75%',
     alignSelf: 'flex-start',
     marginBottom: 10,
-    marginTop: 10,
   },
   chatBubbleText: {
     fontSize: 16,
     color: '#000000',
     lineHeight: 24,
     textAlign: 'left',
+  },
+  chatListContainer: {
+    flex: 1,
+    paddingBottom: 150,
+  },
+  flatListContent: {
+    paddingTop: 20,
+    paddingBottom: 10,
+  },
+  chatBubble: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 12,
+    marginVertical: 5,
+  },
+  chatBubbleUser: {
+    backgroundColor: '#007AFF',
+    alignSelf: 'flex-start',
+    marginBottom: 10,
+  },
+  chatBubbleBot: {
+    backgroundColor: '#FFFFFF',
+    alignSelf: 'flex-end',
+    marginRight: 10,
+  },
+  chatBubbleTextUser: {
+    color: '#FFFFFF',
+  },
+  chatBubbleTextBot: {
+    color: '#000000',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  sendButton: {
+    width: 60,
+    height: 48,
+    backgroundColor: '#007AFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 24,
+  },
+  sendButtonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+  },
+  chatBubbleRight: {
+    backgroundColor: '#E0F7FA',
+    maxWidth: '75%',
+    alignSelf: 'flex-end',
+    marginRight: 10,
+  },
+  flatList: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 150,
   },
 });
 
